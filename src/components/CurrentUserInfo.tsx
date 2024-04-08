6122965c2be0374818490a66615b5158ea687569
Kallum Burgin <Kallb123@users.noreply.github.com>
2024-04-08 10:36:58 +0100

    Tweaked user info panel in case no names are provided

--- src/components/CurrentUserInfo.tsx
+++ src/components/CurrentUserInfo.tsx
@@ -1,14 +1,32 @@
 'use client'
 
 import { useUser } from "@clerk/nextjs";
+import { useEffect, useState } from "react";
 
 export default function CurrentUserInfo() {
     const { user, isLoaded } = useUser();
+    const [visibleName, setVisibleName] = useState('Loading');
+
+    useEffect(() => {
+        if (isLoaded) {
+            if (user?.firstName) {
+                if (user?.lastName) {
+                    setVisibleName(`${user?.firstName} ${user?.lastName}`);
+                } else {
+                    setVisibleName(`${user?.firstName}`);
+                }
+            } else if (user?.username) {
+                setVisibleName(`${user?.username}`);
+            } else {
+                setVisibleName(`${user?.id}`);
+            }
+        }
+    }, [isLoaded, user]);
 
     return (
         <div>
             <p>
-            Hello {user?.firstName} {user?.lastName}. Unlocked: {user?.publicMetadata.unlocked === true ? "Yes" : "No"}
+                Hello {visibleName}. Unlocked: {user?.publicMetadata.unlocked === true ? "Yes" : "No"}
             </p>
         </div>
     );
