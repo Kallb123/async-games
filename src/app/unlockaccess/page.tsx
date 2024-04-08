2d5bbdabbb32238ae001cca02959f79388fd4100
Kallum Burgin <Kallb123@users.noreply.github.com>
2024-04-08 10:35:59 +0100

    Fix access to unlock page

--- src/app/unlockaccess/page.tsx
+++ src/app/unlockaccess/page.tsx
@@ -1,20 +1,33 @@
-import PasswordForm from "@/components/PasswordForm";
-import { auth, currentUser } from "@clerk/nextjs";
-import { redirect, usePathname } from "next/navigation";
-import CurrentUserInfo from "@/components/CurrentUserInfo";
+'use client'
 
-export default async function UnlockAccess() {
+import PasswordForm from "@/components/PasswordForm";
+import { useUser } from "@clerk/nextjs";
+import { usePathname, useRouter } from "next/navigation";
+import CurrentUserInfo from "@/components/CurrentUserInfo";
+import { useEffect } from "react";
+
+export default function UnlockAccess() {
   const pathName = usePathname();
   console.log(`GET ${pathName}`);
-  // Get the userId from auth() -- if null, the user is not signed in
-  const { userId } = auth();
- 
-  if (!userId) {
-    redirect("/login");
-  }
- 
-  // Get the Backend API User object when you need access to the user's information
-  const user = await currentUser();
+  const router = useRouter();
+  
+  const { user, isLoaded } = useUser();
+
+  useEffect(() => {
+    if (isLoaded) {
+        if (!user) {
+            router.push('/login');
+        }
+
+        // Use `user` to render user details or create UI elements
+        const unlocked = user?.publicMetadata.unlocked;
+      
+        if (unlocked !== true) {
+          router.push('/unlockaccess');
+        }
+    }
+  }, [isLoaded]);
+
   // Use `user` to render user details or create UI elements
   const unlocked = user?.publicMetadata.unlocked;
 
