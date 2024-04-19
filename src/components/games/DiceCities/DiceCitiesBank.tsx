d0ff454ad5a5b588cb7f6b298544ab38827744b6
Kallum Burgin <Kallb123@users.noreply.github.com>
2024-04-19 22:48:55 +0100

    Limit what the user can purchase

--- src/components/games/DiceCities/DiceCitiesBank.tsx
+++ src/components/games/DiceCities/DiceCitiesBank.tsx
@@ -1,9 +1,9 @@
-
 import { ICommandResponse } from "@/app/api/game/command/route";
-import { IDiceCitiesCard, IDiceCitiesGameDataResponse, IDiceCitiesGameStateResponse } from "@/games/DiceCities/apiModels";
+import { IDiceCitiesCard, IDiceCitiesCardCountResponse, IDiceCitiesGameDataResponse, IDiceCitiesGameStateResponse } from "@/games/DiceCities/apiModels";
 import { DiceCitiesCards } from "@/games/DiceCities/cards";
 import { uuidString } from "@/utils/apiModels/GameDataApi";
 import { DiceCitiesRequestCardPurchase, IGameCommand } from "@/utils/apiModels/GameLogic";
+import { useUser } from "@clerk/nextjs";
 import { Button } from "react-bootstrap";
 
 interface DiceCitiesBankProps {
@@ -12,12 +12,40 @@ interface DiceCitiesBankProps {
 }
 
 export default function DiceCitiesBank({gameState, submitCommand}: DiceCitiesBankProps) {
+    const { user, isLoaded } = useUser();
+    
     const handlePurchase = async (cardId: uuidString) => {
         const command = new DiceCitiesRequestCardPurchase();
         command.cardId = cardId;
         submitCommand(command, (commandResponse) => {
             console.log(commandResponse);
         });
+    }
+
+    const currentUserState = () => {
+        if (isLoaded && user) {
+            const playerState = gameState.playerStates[user.id];
+            return playerState;
+        }
+        return null;
+    }
+
+    const isDisabled = (card: IDiceCitiesCard, cardCount: IDiceCitiesCardCountResponse) => {
+        if (!gameState.hasRolled || cardCount.amount === 0) {
+            return true;
+        }
+        const userState = currentUserState();
+        if (!userState) {
+            return true;
+        }
+        if (card.cost > userState.money) {
+            return true;
+        }
+        const currentOwnership = userState.cards.find(cc => cc.card == card.cardId)?.amount;
+        if (currentOwnership && currentOwnership >= card.ownLimit) {
+            return true;
+        }
+        return false;
     }
 
     return (
@@ -30,7 +58,7 @@ export default function DiceCitiesBank({gameState, submitCommand}: DiceCitiesBan
                         {gameState && gameState.bankCards.map(cardCount => {
                             const card: IDiceCitiesCard = DiceCitiesCards[cardCount.card.toString()];
                             return (
-                                <li key={cardCount.card.toString()}>{card.title} x{cardCount.amount} <Button onClick={() => {handlePurchase(card.cardId)}} disabled={!gameState.hasRolled || cardCount.amount === 0}>Purchase</Button></li>
+                                <li key={cardCount.card.toString()} title={card.text}>{card.title} x{cardCount.amount} (Cost: {card.cost}) <Button onClick={() => {handlePurchase(card.cardId)}} disabled={isDisabled(card, cardCount)}>Purchase</Button></li>
                             )
                         })}
                     </ul>
