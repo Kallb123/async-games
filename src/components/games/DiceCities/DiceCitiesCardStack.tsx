c323e8e51343cf86c14cf2fa6c824fd7e080d52d
Kallum Burgin <Kallb123@users.noreply.github.com>
2024-04-25 23:01:37 +0100

    Add framer motion to animate cards

--- src/components/games/DiceCities/DiceCitiesCardStack.tsx
+++ src/components/games/DiceCities/DiceCitiesCardStack.tsx
@@ -2,6 +2,7 @@ import { IDiceCitiesCard } from "@/games/DiceCities/apiModels";
 import { useUser } from "@clerk/nextjs";
 import { useEffect } from "react";
 import DiceCitiesCard from "./DiceCitiesCard";
+import { AnimatePresence, motion } from "framer-motion";
 
 const CARD_STACK_OFFSET = 25;
 
@@ -22,13 +23,15 @@ export default function DiceCitiesCardStack({card, amount, disabled}: DiceCities
 
     return (
         <div style={{position: "relative", width: `${162+(amount-1)*CARD_STACK_OFFSET}px`, height: "250px"}}>
-        {
-            [...Array(amount)].map((e, index) => (
-                <div key={index} style={{position: "absolute", left: `${index*CARD_STACK_OFFSET}px`}}>
-                    <DiceCitiesCard card={card} disabled={disabled}></DiceCitiesCard>
-                </div>
-            ))
-        }
+            <AnimatePresence>
+                {
+                    [...Array(amount)].map((e, index) => (
+                        <motion.div key={index} transition={{duration: 1.0, damping: "spring"}} initial={{translateX: "150vw"}} animate={{ translateX: "0vw" }} exit={{ translateX: "150vw" }} style={{position: "absolute", left: `${index*CARD_STACK_OFFSET}px`}}>
+                            <DiceCitiesCard card={card} disabled={disabled}></DiceCitiesCard>
+                        </motion.div>
+                    ))
+                }
+            </AnimatePresence>
         </div>
     );
 }
